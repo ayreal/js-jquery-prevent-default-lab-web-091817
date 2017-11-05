cfa13366f022e3683c211e2b615e9f68a05fc8b3
Ariel Herman <aeh.herman@gmail.com>
2017-11-05 18:16:55 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // call functions here
+  submitForm();
 });
 
 // define functions here
+function submitForm() {
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    var task = $("input").val();
+    let newTask = document.createElement("li");
+    newTask.innerText = task;
+    document.getElementById("list").appendChild(newTask);
+    document.getElementById("item").value = "";
+  });
+}
